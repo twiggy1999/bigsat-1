4236d3ac574bf7b641600847c79ddec4d5f7cce7
ltyLittlePrince <738112037@qq.com>
2019-01-24 13:49:40 +0800

    Add a biased testcase generator and srand(time(0))

--- test_biased.cpp
+++ test_biased.cpp
@@ -2,20 +2,37 @@
 #include <fstream>
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
 using namespace std;
 
 // Only parameters to make changes to
-#define maxVars 100
-#define maxClauses 10000
-#define AvgLitsPerClause 6
+#define maxVars 30
+#define maxClauses 1000
+#define AvgLitsPerClause 5
+#define range 0.1          // double[0~0.5]: About (2*100*range)% variables will be set to extreme (both low and high occurrence)
+#define extreme_cmp 10     // int[1~INF] extreme_cmp = average_occurrence_of_high_ones / average_occurrence_of_low_ones
 
 // Assisting functions
 inline int rand_lits(int avg) { return avg / 2 + (rand() % avg); }
-inline int rand_lit(int max) { int i = 0; while (!i) i = rand() % (2 * max + 1) - max; return i; }
+inline int rand_lit(int max) {
+	int border1 = maxVars * range;
+	int border2 = 2*border1;
+
+	int i = (rand() % maxVars) + 1;
+	int j = rand() % (extreme_cmp + 1);
+	int k = rand() % 2;
+
+	if (1 <= i && i <= border1) {
+		if (j <= 1);
+		else	i+= border1;
+	}
+	
+	i = (k == 1) ? -i : i;
+	return i;
+}
 
 int main()
 {
-	srand(time(0));
         ofstream out("test.txt", ios::out);
 
         out << "c ++++ Auto Generated Input File ++++ " << endl;
@@ -23,8 +40,12 @@ int main()
         out << "c Maximum variables: " << maxVars << endl;
         out << "c Maximum clauses: " << maxClauses << endl;
         out << "c Average literals per clause: " << AvgLitsPerClause << endl;
+	out << "c Variables from 1 to " << (int)(maxVars * range) << " will significantly occur less." << endl;
+	out << "c Variables from " << (int)(maxVars * range) + 1 << " to " << 2*(int)(maxVars * range) << " will frequently appear." << endl; 	
 
         out << "p cnf " << maxVars << " " << maxClauses << endl;
+
+	srand(time(0));
 
         for (int i = 0; i < maxClauses; i++) {
                 int lits = rand_lits(AvgLitsPerClause);
